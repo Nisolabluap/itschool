314497f5530b1a01ff7641270e6f5f41baea7c2a
Nisolabluap <balosin.paul@protonmail.com>
2023-08-01 00:56:26 +0300

    First commit, v0.0.1

---
+++ src/session2/Practice.java
@@ -0,0 +1,16 @@
+package session2;
+
+import java.util.Scanner;
+
+public class Practice {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String myName = scan.nextLine();
+
+        System.out.println("Hello, " + myName + "," + "\nWelcome to arithmetic class.");
+
+        // You can add more code for arithmetic operations here if needed.
+    }
+}
